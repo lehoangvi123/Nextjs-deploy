fd7bc0c0e5dec95aaad693180e4ea386807d97f2
Projdct-Web-Shopee-Website <lehoangvi.work@gmail.com>
2025-05-22 08:21:39 +0700

    Update Footer.tsx

--- my-portfolio/components/Footer.tsx
+++ my-portfolio/components/Footer.tsx
@@ -3,7 +3,7 @@ export function Footer() {
     <footer className="bg-gray-800 text-white p-4 text-center">
       <p>© {new Date().getFullYear()} Lê Hoàng Vĩ. All rights reserved.</p>
       <p>
-        Email: <a href="mailto:lehoangvi.work@gmail.com" className="underline">lehoangvi.work@gmail.com</a> | Phone: 0824318868
+        Email: <a href="mailto:lehoangvi.work@gmail.com" className="underline">lehoangvi.work@gmail.com</a> | Phone: 0824318186
       </p>
     </footer>
   );
